b6eafeb8641de1d8b2e188de8b542cedc23c7c98
Vansa Pha <vansapha@vansas-mac.local>
2016-12-22 11:09:30 +0700

    7th

--- Read Contact/classes/module/contact/view/MainContactTableViewController.swift
+++ Read Contact/classes/module/contact/view/MainContactTableViewController.swift
@@ -38,17 +38,15 @@ class MainContactTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        return false
+        return true
     }
 
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
-            
+            mySelectedContact.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
     
     // Override to support rearranging the table view.
